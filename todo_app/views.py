82f747adb27f224c62334ccc3c9cc0f5396f1b52
shiblynk <shiblynk407@gmail.com>
2026-03-08 22:37:00 +0530

    Added Task Creation Feature

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Task
 
 def task_list(request):
+    if request.method == 'POST':
+        task_title = request.POST.get('title')
+        if task_title:
+            Task.objects.create(title=task_title)
+        
+        return redirect('task_list')
+            
     tasks = Task.objects.all()
     return render(request, 'todo/task_list.html', {'tasks' : tasks})
+
