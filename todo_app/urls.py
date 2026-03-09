520e17c45ad6cc947a269bbc8e1605e4711f5d8c
shiblynk <shiblynk407@gmail.com>
2026-03-09 06:34:55 +0530

    Added Task Delete Feature

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import task_list
+from .views import delete_task, task_list
 
 
 urlpatterns = [
     path('', task_list, name='task_list'),
+    path('delete/<int:task_id>/',delete_task, name='delete_task'),
 ]
 
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from .models import Task
 
 def task_list(request):
@@ -11,4 +11,7 @@ def task_list(request):
             
     tasks = Task.objects.all()
     return render(request, 'todo/task_list.html', {'tasks' : tasks})
-
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.delete()
+    return redirect('task_list')
