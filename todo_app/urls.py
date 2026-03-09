3e154365cf22063a8fce20c587f876d0a0f94761
shiblynk <shiblynk407@gmail.com>
2026-03-09 06:48:52 +0530

    Adde Completed Feature

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import delete_task, task_list
+from .views import delete_task, task_list, toggle_complete
 
 
 urlpatterns = [
     path('', task_list, name='task_list'),
     path('delete/<int:task_id>/',delete_task, name='delete_task'),
+    path('complete/<int:task_id>/',toggle_complete, name='toggle_complete'),
 ]
 
--- todo_app/views.py
+++ todo_app/views.py
@@ -15,3 +15,8 @@ def delete_task(request, task_id):
     task = get_object_or_404(Task, id=task_id)
     task.delete()
     return redirect('task_list')
+def toggle_complete(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.completed = not task.completed
+    task.save()
+    return redirect('task_list')
